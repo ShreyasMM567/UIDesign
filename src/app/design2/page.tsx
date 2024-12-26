8024dec45e33ad883feb8aeb5ce0e9b86bce77f0
ShreyasMM567 <shreyasmm567@gmail.com>
2024-12-26 21:18:56 +0530

    finishing design

--- src/app/design2/page.tsx
+++ src/app/design2/page.tsx
@@ -4,8 +4,8 @@ export default function Design2() {
   return (
     <main className="overflow-x-hidden flex mx-auto place-items-center max-w-[1440px]">
       <div className="grid grid-cols-12">
-        <div className="col-span-7 row-start-2 row-span-1 relative left-[25%] bottom-[30%] flex flex-col gap-5 p-10">
-          <h1 className="m-0 text-[#363830] font-bold text-8xl">
+        <div className="col-span-7 row-start-1 row-span-1 relative left-[30%] flex flex-col p-[5%]">
+          <h1 className="m-0 text-[#363830] font-bold text-[88px]">
             Interior design
           </h1>
           <div className="">
@@ -16,7 +16,7 @@ export default function Design2() {
             </span>
           </div>
           <div>
-            <button className="text-2xl text-white bg-[#363830] rounded-full p-4 w-[25%]">
+            <button className="mt-3 text-2xl text-white bg-[#363830] rounded-full p-4 w-[25%]">
               Explore
             </button>
           </div>
@@ -42,16 +42,42 @@ export default function Design2() {
           />
         </div>
 
-        <div className="relative right-[20%] row-start-3 col-span-7  text-[#363830] bg-white/30 h-[50%] z-10 p-[6%] rounded-full mt-[10%] grid grid-cols-12 ">
-            <div className="flex flex-col">
-
+        <div className="relative right-[15%] row-start-3 col-span-7">
+          <div className="absolute text-[#363830] bg-white/30 z-10 px-[10%] py-[5%] rounded-full mt-[10%] grid grid-cols-12 gap-[68px]">
+            <div className="flex flex-col col-span-4 gap-10">
+              <div>
+                <span className="border-b-2 border-[#363830] text-2xl font-bold pb-5">5.8</span>
+              </div>
+              <div>
+                <span className="text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Faucibus in libero risus semper habitant arcu eget.
+                </span>
+              </div>
             </div>
-            <div className="flex flex-col">
-                
+            <div className="flex flex-col col-span-4 gap-10">
+              <div>
+                <span className="border-b-2 border-[#363830] text-2xl font-bold pb-5">99+</span>
+              </div>
+              <div>
+                <span className="text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Faucibus in libero risus semper habitant arcu eget.
+                </span>
+              </div>
             </div>
-            <div className="flex flex-col">
-                
+            <div className="flex flex-col col-span-4 gap-10">
+              <div>
+                <span className="border-b-2 border-[#363830] text-2xl font-bold pb-5">110k</span>
+              </div>
+              <div>
+                <span className="text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Faucibus in libero risus semper habitant arcu eget.
+                </span>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </main>
